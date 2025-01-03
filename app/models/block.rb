beb58ced52201534aaa7a90d0279c1addbe38244
Shakhboz <Shfazliddinov1940@gmail.com>
2025-01-03 15:10:59 +0500

    added css

--- app/models/block.rb
+++ app/models/block.rb
@@ -3,4 +3,17 @@ class Block < ApplicationRecord
   has_many :floors
   has_many :apartments
   has_many :apartment_numbers
+
+  def number_of_rooms
+    apartments.count
+  end
+
+  def total_square
+    square = 0
+    apartment_numbers.each do |apartment_number|
+      square += apartment_number.square * floors.count
+    end
+
+    square
+  end
 end
