370ef0d199884ac21a3fdce840bf76ca1ea5d9b4
Shakhboz <Shfazliddinov1940@gmail.com>
2025-02-02 17:10:18 +0500

    m

--- app/models/top_up.rb
+++ app/models/top_up.rb
@@ -3,4 +3,16 @@ class TopUp < ApplicationRecord
   belongs_to :user
   validates_presence_of :amount
   enum :top_up_type, [ :cash, :card, :bank, :mix ], suffix: true
+
+  after_create :send_message
+
+  private
+
+  def send_message
+    message =
+      "<b>Оплата</b>" \n
+      "<b>Cумма: </b> #{amount}"
+
+    SendMessageJob.perform_later(message)
+  end
 end
