a696f2e18e6b37c2ee79375f023acf4c50b5b091
Shakhboz <Shfazliddinov1940@gmail.com>
2025-02-25 22:40:40 +0500

    m

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -93,6 +93,13 @@ class ContractsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def contract_params
-      params.expect(contract: [ :payment_day, :first_payment_in_cash, :first_payment_in_percent, :number_of_months, :fullname, :pinfl, :passport_number, :user_id, :status, :price_per_square, :apartment_id, :contract_number, :phone_number ])
+      params.expect(
+        contract: [
+          :payment_day, :first_payment_in_cash, :first_payment_in_percent, :number_of_months,
+          :fullname, :pinfl, :passport_number, :user_id, :status, :price_per_square,
+          :apartment_id, :contract_number, :phone_number, :address, :pinfl, :extra_phone_number,
+          :deadline, :passport_date
+        ]
+      )
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_02_041308) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_25_163248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -58,6 +58,10 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_02_041308) do
     t.string "phone_number"
     t.integer "contract_number"
     t.integer "payment_day"
+    t.date "passport_date"
+    t.date "deadline"
+    t.string "address"
+    t.string "extra_phone_number"
     t.index ["apartment_id"], name: "index_contracts_on_apartment_id"
     t.index ["user_id"], name: "index_contracts_on_user_id"
   end
@@ -68,6 +72,38 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_02_041308) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["block_id"], name: "index_floors_on_block_id"
+  end
+
+  create_table "mobility_string_translations", force: :cascade do |t|
+    t.string "locale", null: false
+    t.string "key", null: false
+    t.string "value"
+    t.string "translatable_type"
+    t.bigint "translatable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["translatable_id", "translatable_type", "key"], name: "index_mobility_string_translations_on_translatable_attribute"
+    t.index ["translatable_id", "translatable_type", "locale", "key"], name: "index_mobility_string_translations_on_keys", unique: true
+    t.index ["translatable_type", "key", "value", "locale"], name: "index_mobility_string_translations_on_query_keys"
+  end
+
+  create_table "mobility_text_translations", force: :cascade do |t|
+    t.string "locale", null: false
+    t.string "key", null: false
+    t.text "value"
+    t.string "translatable_type"
+    t.bigint "translatable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["translatable_id", "translatable_type", "key"], name: "index_mobility_text_translations_on_translatable_attribute"
+    t.index ["translatable_id", "translatable_type", "locale", "key"], name: "index_mobility_text_translations_on_keys", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "price_calculators", force: :cascade do |t|
