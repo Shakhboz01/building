660a8bc3492b7c962693fa4f7430a77384d8d2e5
Shakhboz <Shfazliddinov1940@gmail.com>
2025-03-01 16:01:02 +0500

    added some changes

--- app/models/room_square.rb
+++ app/models/room_square.rb
@@ -1,5 +1,4 @@
 class RoomSquare < ApplicationRecord
   belongs_to :apartment_number, optional: true
   belongs_to :specific_apartment_number, optional: true
-  validates_uniqueness_of :name, scope: :apartment_number_id
 end
