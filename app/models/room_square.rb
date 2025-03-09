0d24dab5c156b52ae435fa7495e881829012d761
Shakhboz <Shfazliddinov1940@gmail.com>
2025-03-09 16:19:26 +0500

    some change

--- app/models/room_square.rb
+++ app/models/room_square.rb
@@ -1,4 +1,4 @@
 class RoomSquare < ApplicationRecord
   belongs_to :apartment_number, optional: true
-  belongs_to :specific_apartment_number, optional: true
+  belongs_to :specific_epartment_number, optional: true
 end
